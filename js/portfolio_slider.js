24bfd219e84ae6d2e594630b8e5cd6ba1546ef96
INANE <inaneamine@gmail.com>
2016-05-23 00:25:06 +0200

    Site ready to publish

---
+++ js/portfolio_slider.js
@@ -0,0 +1,156 @@
+$(function(){
+$("#elastic_grid_demo").elastic_grid({	
+	'hoverDirection': false,
+	'hoverDelay': 0,
+	'hoverInverse': false,
+	'expandingSpeed': 500,
+	'expandingHeight': 500,
+	'items' :
+		[
+			{
+			    'title': 'Le coiffeur au talent ',
+			'description'   : '',
+			'thumbnail': ['img/portfolio/small/1.jpg', 'img/portfolio/small/6.jpg', 'img/portfolio/small/9.jpg', 'img/portfolio/small/12.jpg', 'img/portfolio/small/15.jpg', 'img/portfolio/small/19.jpg'],
+			'large': ['img/portfolio/large/1.jpg', 'img/portfolio/large/6.jpg', 'img/portfolio/large/9.jpg', 'img/portfolio/large/12.jpg', 'img/portfolio/large/15.jpg', 'img/portfolio/large/19.jpg'],
+			'button_list'   :	[],
+			'tags'  : ['selfie', 'Tout']
+			},
+
+			{
+			    'title': 'Merci atoi ali',
+			'description'   : '',
+			'thumbnail': ['img/portfolio/small/2.jpg', 'img/portfolio/small/7.jpg', 'img/portfolio/small/8.jpg', 'img/portfolio/small/10.jpg', 'img/portfolio/small/11.jpg', 'img/portfolio/small/13.jpg', 'img/portfolio/small/14.jpg', 'img/portfolio/small/16.jpg'],
+			'large': ['img/portfolio/large/2.jpg', 'img/portfolio/large/7.jpg', 'img/portfolio/large/8.jpg', 'img/portfolio/large/10.jpg', 'img/portfolio/large/11.jpg', 'img/portfolio/large/13.jpg', 'img/portfolio/large/14.jpg', 'img/portfolio/large/16.jpg'],
+			'button_list': [],
+			'tags': ['client', 'Tout']
+			},
+
+			{
+			    'title': 'New style ',
+			'description'   : '',
+			'thumbnail': ['img/portfolio/small/3.jpg', 'img/portfolio/small/4.jpg', 'img/portfolio/small/5.jpg', 'img/portfolio/small/17.jpg', 'img/portfolio/small/18.jpg'],
+			'large': ['img/portfolio/large/3.jpg', 'img/portfolio/large/4.jpg', 'img/portfolio/large/5.jpg', 'img/portfolio/large17.jpg', 'img/portfolio/large/18.jpg'],
+			'button_list': [],
+			'tags': ['evenement', 'Tout']
+			},
+
+			{
+			    'title': 'Kreamer Gokhan',
+			'description'   : '',
+			'thumbnail': ['img/portfolio/small/4.jpg', 'img/portfolio/small/3.jpg', 'img/portfolio/small/5.jpg', 'img/portfolio/small/17.jpg', 'img/portfolio/small/18.jpg'],
+			'large': ['img/portfolio/large/4.jpg', 'img/portfolio/large/3.jpg', 'img/portfolio/large/5.jpg', 'img/portfolio/large17.jpg', 'img/portfolio/large/18.jpg'],
+			'button_list': [],
+			'tags': ['evenement', 'Tout']
+			},
+
+			{
+			    'title': 'Galerieslafayette',
+			'description'   : '',
+			'thumbnail': ['img/portfolio/small/5.jpg', 'img/portfolio/small/3.jpg', 'img/portfolio/small/4.jpg', 'img/portfolio/small/17.jpg', 'img/portfolio/small/18.jpg'],
+			'large': ['img/portfolio/large/5.jpg', 'img/portfolio/large/3.jpg', 'img/portfolio/large/4.jpg', 'img/portfolio/large17.jpg', 'img/portfolio/large/18.jpg'],
+			'button_list': [],
+			'tags': ['evenement', 'Tout']
+			},
+
+			{
+			    'title': 'Chez kreamer',
+			'description'   : '',
+			'thumbnail': ['img/portfolio/small/6.jpg', 'img/portfolio/small/1.jpg', 'img/portfolio/small/9.jpg', 'img/portfolio/small/12.jpg', 'img/portfolio/small/15.jpg', 'img/portfolio/small/19.jpg'],
+			'large': ['img/portfolio/large/6.jpg', 'img/portfolio/large/1.jpg', 'img/portfolio/large/9.jpg', 'img/portfolio/large/12.jpg', 'img/portfolio/large/15.jpg', 'img/portfolio/large/19.jpg'],
+			'button_list': [],
+			'tags': ['selfie', 'Tout']
+			},
+
+			{
+			    'title': 'New sytle theo',
+			'description'   : '',
+			'thumbnail': ['img/portfolio/small/7.jpg', 'img/portfolio/small/2.jpg', 'img/portfolio/small/8.jpg', 'img/portfolio/small/10.jpg', 'img/portfolio/small/11.jpg', 'img/portfolio/small/13.jpg', 'img/portfolio/small/14.jpg', 'img/portfolio/small/16.jpg'],
+			'large': ['img/portfolio/large/7.jpg', 'img/portfolio/large/2.jpg', 'img/portfolio/large/8.jpg', 'img/portfolio/large/10.jpg', 'img/portfolio/large/11.jpg', 'img/portfolio/large/13.jpg', 'img/portfolio/large/14.jpg', 'img/portfolio/large/16.jpg'],
+			'button_list': [],
+			'tags': ['client', 'Tout']
+			},
+
+			{
+			    'title': 'New sytle Philippe',
+			'description'   : '',
+			'thumbnail': ['img/portfolio/small/8.jpg', 'img/portfolio/small/2.jpg', 'img/portfolio/small/7.jpg', 'img/portfolio/small/10.jpg', 'img/portfolio/small/11.jpg', 'img/portfolio/small/13.jpg', 'img/portfolio/small/14.jpg', 'img/portfolio/small/16.jpg'],
+			'large': ['img/portfolio/large/8.jpg', 'img/portfolio/large/2.jpg', 'img/portfolio/large/7.jpg', 'img/portfolio/large/10.jpg', 'img/portfolio/large/11.jpg', 'img/portfolio/large/13.jpg', 'img/portfolio/large/14.jpg', 'img/portfolio/large/16.jpg'],
+			'button_list': [],
+			'tags': ['client', 'Tout']
+			},
+
+			{
+			    'title': 'Kreamer Gokhan',
+			'description'   : '',
+			'thumbnail': ['img/portfolio/small/9.jpg', 'img/portfolio/small/15.jpg', 'img/portfolio/small/12.jpg', 'img/portfolio/small/1.jpg', 'img/portfolio/small/6.jpg', 'img/portfolio/small/19.jpg'],
+			'large': ['img/portfolio/large/9.jpg', 'img/portfolio/large/15.jpg', 'img/portfolio/large/12.jpg', 'img/portfolio/large/1.jpg', 'img/portfolio/large/6.jpg', 'img/portfolio/large/19.jpg'],
+			'button_list': [],
+			'tags': ['selfie', 'Tout']
+			},
+
+			{
+			'title' : 'New style Benjamin',
+			'description'   : '',
+			'thumbnail': ['img/portfolio/small/10.jpg', 'img/portfolio/small/2.jpg', 'img/portfolio/small/7.jpg', 'img/portfolio/small/8.jpg', 'img/portfolio/small/11.jpg', 'img/portfolio/small/13.jpg', 'img/portfolio/small/14.jpg', 'img/portfolio/small/16.jpg'],
+			'large': ['img/portfolio/large/10.jpg', 'img/portfolio/large/2.jpg', 'img/portfolio/large/7.jpg', 'img/portfolio/large/8.jpg', 'img/portfolio/large/11.jpg', 'img/portfolio/large/13.jpg', 'img/portfolio/large/14.jpg', 'img/portfolio/large/16.jpg'],
+			'button_list': [],
+			'tags': ['client', 'Tout']
+			},
+
+			{
+			    'title': 'New style suleyman',
+			'description'   : '',
+			'thumbnail': ['img/portfolio/small/11.jpg', 'img/portfolio/small/2.jpg', 'img/portfolio/small/7.jpg', 'img/portfolio/small/8.jpg', 'img/portfolio/small/10.jpg', 'img/portfolio/small/13.jpg', 'img/portfolio/small/14.jpg', 'img/portfolio/small/16.jpg'],
+			'large': ['img/portfolio/large/11.jpg', 'img/portfolio/large/2.jpg', 'img/portfolio/large/7.jpg', 'img/portfolio/large/8.jpg', 'img/portfolio/large/10.jpg', 'img/portfolio/large/13.jpg', 'img/portfolio/large/14.jpg', 'img/portfolio/large/16.jpg'],
+			'button_list': [],
+			'tags': ['client', 'Tout']
+			},
+
+			{
+			    'title': 'Merci begendigin icin',
+			'description'   : '',
+			'thumbnail': ['img/portfolio/small/12.jpg', 'img/portfolio/small/1.jpg', 'img/portfolio/small/6.jpg', 'img/portfolio/small/9.jpg', 'img/portfolio/small/15.jpg', 'img/portfolio/small/19.jpg'],
+			'large': ['img/portfolio/large/12.jpg', 'img/portfolio/large/1.jpg', 'img/portfolio/large/6.jpg', 'img/portfolio/large/9.jpg', 'img/portfolio/large/15.jpg', 'img/portfolio/large/19.jpg'],
+			'button_list': [],
+			'tags': ['selfie', 'Tout']
+			},
+
+			{
+			'title' : 'New Style',
+			'description'   : '',
+			'thumbnail': ['img/portfolio/small/13.jpg', 'img/portfolio/small/2.jpg', 'img/portfolio/small/7.jpg', 'img/portfolio/small/8.jpg', 'img/portfolio/small/10.jpg', 'img/portfolio/small/11.jpg', 'img/portfolio/small/14.jpg', 'img/portfolio/small/16.jpg'],
+			'large': ['img/portfolio/large/13.jpg', 'img/portfolio/large/2.jpg', 'img/portfolio/large/7.jpg', 'img/portfolio/large/8.jpg', 'img/portfolio/large/10.jpg', 'img/portfolio/large/11.jpg', 'img/portfolio/large/14.jpg', 'img/portfolio/large/16.jpg'],
+			'button_list': [],
+			'tags': ['client', 'Tout']
+			},
+
+			{
+			    'title': 'New Style',
+			    'description': '',
+			    'thumbnail': ['img/portfolio/small/14.jpg', 'img/portfolio/small/2.jpg', 'img/portfolio/small/7.jpg', 'img/portfolio/small/8.jpg', 'img/portfolio/small/10.jpg', 'img/portfolio/small/11.jpg', 'img/portfolio/small/13.jpg', 'img/portfolio/small/16.jpg'],
+			    'large': ['img/portfolio/large/14.jpg', 'img/portfolio/large/2.jpg', 'img/portfolio/large/7.jpg', 'img/portfolio/large/8.jpg', 'img/portfolio/large/10.jpg', 'img/portfolio/large/11.jpg', 'img/portfolio/large/13.jpg', 'img/portfolio/large/16.jpg'],
+			    'button_list': [],
+			    'tags': ['client', 'Tout']
+			},
+
+			{
+			    'title': 'Salon Ciseaux Or',
+			    'description': '',
+			    'thumbnail': ['img/portfolio/small/15.jpg', 'img/portfolio/small/1.jpg', 'img/portfolio/small/6.jpg', 'img/portfolio/small/9.jpg', 'img/portfolio/small/12.jpg', 'img/portfolio/small/19.jpg'],
+			    'large': ['img/portfolio/large/15.jpg', 'img/portfolio/large/1.jpg', 'img/portfolio/large/6.jpg', 'img/portfolio/large/9.jpg', 'img/portfolio/large/12.jpg', 'img/portfolio/large/19.jpg'],
+			    'button_list': [],
+			    'tags': ['selfie', 'Tout']
+			},
+
+			{
+			    'title': 'Equipe lOreal',
+			    'description': '',
+			    'thumbnail': ['img/portfolio/small/17.jpg', 'img/portfolio/small/18.jpg', 'img/portfolio/small/4.jpg', 'img/portfolio/small/5.jpg', 'img/portfolio/small/3.jpg'],
+			    'large': ['img/portfolio/large/17.jpg', 'img/portfolio/large/18.jpg', 'img/portfolio/large/4.jpg', 'img/portfolio/large5.jpg', 'img/portfolio/large/3.jpg'],
+			    'button_list': [],
+			    'tags': ['evenement', 'Tout']
+			}
+	 
+		]
+	});
+});
