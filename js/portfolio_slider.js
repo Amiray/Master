1229fd329e7a49c3efd92ee7e82bcc6ce3b285dd
INANE <inaneamine@gmail.com>
2016-07-30 12:47:40 +0200

    modification des images

--- js/portfolio_slider.js
+++ js/portfolio_slider.js
@@ -10,8 +10,8 @@ $("#elastic_grid_demo").elastic_grid({
 			{
 			    'title': 'Le coiffeur au talent ',
 			'description'   : '',
-			'thumbnail': ['img/portfolio/small/1.jpg', 'img/portfolio/small/6.jpg', 'img/portfolio/small/9.jpg', 'img/portfolio/small/12.jpg', 'img/portfolio/small/15.jpg', 'img/portfolio/small/19.jpg'],
-			'large': ['img/portfolio/large/1.jpg', 'img/portfolio/large/6.jpg', 'img/portfolio/large/9.jpg', 'img/portfolio/large/12.jpg', 'img/portfolio/large/15.jpg', 'img/portfolio/large/19.jpg'],
+			'thumbnail': ['img/portfolio/small/33.jpg', 'img/portfolio/small/34.jpg','img/portfolio/small/1.jpg', 'img/portfolio/small/6.jpg', 'img/portfolio/small/9.jpg', 'img/portfolio/small/12.jpg', 'img/portfolio/small/15.jpg', 'img/portfolio/small/19.jpg'],
+			'large': ['img/portfolio/large/33.jpg', 'img/portfolio/large/34.jpg','img/portfolio/large/1.jpg', 'img/portfolio/large/6.jpg', 'img/portfolio/large/9.jpg', 'img/portfolio/large/12.jpg', 'img/portfolio/large/15.jpg', 'img/portfolio/large/19.jpg'],
 			'button_list'   :	[],
 			'tags'  : ['selfie', 'Tout']
 			},
@@ -23,42 +23,6 @@ $("#elastic_grid_demo").elastic_grid({
 			'large': ['img/portfolio/large/2.jpg', 'img/portfolio/large/7.jpg', 'img/portfolio/large/8.jpg', 'img/portfolio/large/10.jpg', 'img/portfolio/large/11.jpg', 'img/portfolio/large/13.jpg', 'img/portfolio/large/14.jpg', 'img/portfolio/large/16.jpg'],
 			'button_list': [],
 			'tags': ['client', 'Tout']
-			},
-
-			{
-			    'title': 'New style ',
-			'description'   : '',
-			'thumbnail': ['img/portfolio/small/3.jpg', 'img/portfolio/small/4.jpg', 'img/portfolio/small/5.jpg', 'img/portfolio/small/17.jpg', 'img/portfolio/small/18.jpg'],
-			'large': ['img/portfolio/large/3.jpg', 'img/portfolio/large/4.jpg', 'img/portfolio/large/5.jpg', 'img/portfolio/large17.jpg', 'img/portfolio/large/18.jpg'],
-			'button_list': [],
-			'tags': ['evenement', 'Tout']
-			},
-
-			{
-			    'title': 'Kreamer Gokhan',
-			'description'   : '',
-			'thumbnail': ['img/portfolio/small/4.jpg', 'img/portfolio/small/3.jpg', 'img/portfolio/small/5.jpg', 'img/portfolio/small/17.jpg', 'img/portfolio/small/18.jpg'],
-			'large': ['img/portfolio/large/4.jpg', 'img/portfolio/large/3.jpg', 'img/portfolio/large/5.jpg', 'img/portfolio/large17.jpg', 'img/portfolio/large/18.jpg'],
-			'button_list': [],
-			'tags': ['evenement', 'Tout']
-			},
-
-			{
-			    'title': 'Galerieslafayette',
-			'description'   : '',
-			'thumbnail': ['img/portfolio/small/5.jpg', 'img/portfolio/small/3.jpg', 'img/portfolio/small/4.jpg', 'img/portfolio/small/17.jpg', 'img/portfolio/small/18.jpg'],
-			'large': ['img/portfolio/large/5.jpg', 'img/portfolio/large/3.jpg', 'img/portfolio/large/4.jpg', 'img/portfolio/large17.jpg', 'img/portfolio/large/18.jpg'],
-			'button_list': [],
-			'tags': ['evenement', 'Tout']
-			},
-
-			{
-			    'title': 'Chez kreamer',
-			'description'   : '',
-			'thumbnail': ['img/portfolio/small/6.jpg', 'img/portfolio/small/1.jpg', 'img/portfolio/small/9.jpg', 'img/portfolio/small/12.jpg', 'img/portfolio/small/15.jpg', 'img/portfolio/small/19.jpg'],
-			'large': ['img/portfolio/large/6.jpg', 'img/portfolio/large/1.jpg', 'img/portfolio/large/9.jpg', 'img/portfolio/large/12.jpg', 'img/portfolio/large/15.jpg', 'img/portfolio/large/19.jpg'],
-			'button_list': [],
-			'tags': ['selfie', 'Tout']
 			},
 
 			{
@@ -80,15 +44,6 @@ $("#elastic_grid_demo").elastic_grid({
 			},
 
 			{
-			    'title': 'Kreamer Gokhan',
-			'description'   : '',
-			'thumbnail': ['img/portfolio/small/9.jpg', 'img/portfolio/small/15.jpg', 'img/portfolio/small/12.jpg', 'img/portfolio/small/1.jpg', 'img/portfolio/small/6.jpg', 'img/portfolio/small/19.jpg'],
-			'large': ['img/portfolio/large/9.jpg', 'img/portfolio/large/15.jpg', 'img/portfolio/large/12.jpg', 'img/portfolio/large/1.jpg', 'img/portfolio/large/6.jpg', 'img/portfolio/large/19.jpg'],
-			'button_list': [],
-			'tags': ['selfie', 'Tout']
-			},
-
-			{
 			'title' : 'New style Benjamin',
 			'description'   : '',
 			'thumbnail': ['img/portfolio/small/10.jpg', 'img/portfolio/small/2.jpg', 'img/portfolio/small/7.jpg', 'img/portfolio/small/8.jpg', 'img/portfolio/small/11.jpg', 'img/portfolio/small/13.jpg', 'img/portfolio/small/14.jpg', 'img/portfolio/small/16.jpg'],
@@ -107,15 +62,6 @@ $("#elastic_grid_demo").elastic_grid({
 			},
 
 			{
-			    'title': 'Merci begendigin icin',
-			'description'   : '',
-			'thumbnail': ['img/portfolio/small/12.jpg', 'img/portfolio/small/1.jpg', 'img/portfolio/small/6.jpg', 'img/portfolio/small/9.jpg', 'img/portfolio/small/15.jpg', 'img/portfolio/small/19.jpg'],
-			'large': ['img/portfolio/large/12.jpg', 'img/portfolio/large/1.jpg', 'img/portfolio/large/6.jpg', 'img/portfolio/large/9.jpg', 'img/portfolio/large/15.jpg', 'img/portfolio/large/19.jpg'],
-			'button_list': [],
-			'tags': ['selfie', 'Tout']
-			},
-
-			{
 			'title' : 'New Style',
 			'description'   : '',
 			'thumbnail': ['img/portfolio/small/13.jpg', 'img/portfolio/small/2.jpg', 'img/portfolio/small/7.jpg', 'img/portfolio/small/8.jpg', 'img/portfolio/small/10.jpg', 'img/portfolio/small/11.jpg', 'img/portfolio/small/14.jpg', 'img/portfolio/small/16.jpg'],
@@ -131,15 +77,6 @@ $("#elastic_grid_demo").elastic_grid({
 			    'large': ['img/portfolio/large/14.jpg', 'img/portfolio/large/2.jpg', 'img/portfolio/large/7.jpg', 'img/portfolio/large/8.jpg', 'img/portfolio/large/10.jpg', 'img/portfolio/large/11.jpg', 'img/portfolio/large/13.jpg', 'img/portfolio/large/16.jpg'],
 			    'button_list': [],
 			    'tags': ['client', 'Tout']
-			},
-
-			{
-			    'title': 'Salon Ciseaux Or',
-			    'description': '',
-			    'thumbnail': ['img/portfolio/small/15.jpg', 'img/portfolio/small/1.jpg', 'img/portfolio/small/6.jpg', 'img/portfolio/small/9.jpg', 'img/portfolio/small/12.jpg', 'img/portfolio/small/19.jpg'],
-			    'large': ['img/portfolio/large/15.jpg', 'img/portfolio/large/1.jpg', 'img/portfolio/large/6.jpg', 'img/portfolio/large/9.jpg', 'img/portfolio/large/12.jpg', 'img/portfolio/large/19.jpg'],
-			    'button_list': [],
-			    'tags': ['selfie', 'Tout']
 			},
 
 			{
@@ -174,6 +111,33 @@ $("#elastic_grid_demo").elastic_grid({
 			    'description': '',
 			    'thumbnail': ['img/portfolio/small/23.jpg', 'img/portfolio/small/21.jpg', 'img/portfolio/small/22.jpg', 'img/portfolio/small/20.jpg'],
 			    'large': ['img/portfolio/large/23.jpg', 'img/portfolio/large/21.jpg', 'img/portfolio/large/22.jpg', 'img/portfolio/large/20.jpg'],
+			    'button_list': [],
+			    'tags': ['evenement', 'Tout']
+			},
+
+			{
+			    'title': 'Strasbourg Fashion Week',
+			    'description': '',
+			    'thumbnail': ['img/portfolio/small/24.jpg', 'img/portfolio/small/25.jpg', 'img/portfolio/small/26.jpg', 'img/portfolio/small/27.jpg', 'img/portfolio/small/31.jpg', 'img/portfolio/small/32.jpg'],
+			    'large': ['img/portfolio/large/24.jpg', 'img/portfolio/large/25.jpg', 'img/portfolio/large/26.jpg', 'img/portfolio/large/27.jpg', 'img/portfolio/large/31.jpg', 'img/portfolio/large/32.jpg'],
+			    'button_list': [],
+			    'tags': ['evenement', 'Tout']
+			},
+
+			{
+			    'title': 'David Gokhan Photo Shooting',
+			    'description': '',
+			    'thumbnail': ['img/portfolio/small/36.jpg', 'img/portfolio/small/35.jpg', 'img/portfolio/small/37.jpg', 'img/portfolio/small/38.jpg', 'img/portfolio/small/39.jpg', 'img/portfolio/small/40.jpg', 'img/portfolio/small/41.jpg'],
+			    'large': ['img/portfolio/large/36.jpg', 'img/portfolio/large/35.jpg', 'img/portfolio/large/37.jpg', 'img/portfolio/large/38.jpg', 'img/portfolio/large/39.jpg', 'img/portfolio/large/40.jpg', 'img/portfolio/large/41.jpg'],
+			    'button_list': [],
+			    'tags': ['evenement', 'Tout']
+			},
+
+			{
+			    'title': 'Shooting Photo Barber Shop',
+			    'description': '',
+			    'thumbnail': ['img/portfolio/small/30.jpg', 'img/portfolio/small/29.jpg', 'img/portfolio/small/28.jpg', 'img/portfolio/small/27.jpg', 'img/portfolio/small/31.jpg'],
+			    'large': ['img/portfolio/large/30.jpg', 'img/portfolio/large/29.jpg', 'img/portfolio/large/28.jpg', 'img/portfolio/large/27.jpg', 'img/portfolio/large/31.jpg'],
 			    'button_list': [],
 			    'tags': ['evenement', 'Tout']
 			}
